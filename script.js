605227e0568acfce5e8885e3ecfff901bcded135
zeldabrufal <zelda@live.co.uk>
2025-03-10 12:57:30 +0000

    yes

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function showMessage() {
-    document.getElementById("message").innerText = "Hello! Thanks for visiting.";
-}
+function loadNavbar() {
+
+    fetch('navbar.html')
+      .then(response => response.text())  // Read the file as text
+      .then(data => {
+        // Inject the loaded navbar content into the div with id "navbar-container"
+        document.getElementById('navbar-container').innerHTML = data;
+      })
+      .catch(error => {
+        // If there's an error (e.g., file not found), log the error
+        console.error('Error loading the navbar:', error);
+      });
+  }
+  
